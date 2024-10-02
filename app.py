d3c51ed36d27a973bf226238eb2bef018cd9ff0d
ViktoriiaSytnik <ssytnik@kse.org.ua>
2024-10-02 21:57:13 +0200

    Updated index.html and app

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, render_template, request
-
-
-def convert(weight, unit):
-    if unit == "pounds":
-        weight = round(float(weight) / 2.205, 2)
-        unit = "kilograms"
-    elif unit == "kilograms":
-        weight = round(float(weight) * 2.205, 2)
-        unit = "pound"
-
-    return weight, unit
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        weight = request.form['weight']
-        unit = request.form['unit']
-        print(f"{weight} {unit}")
-        weight, unit = convert(weight, unit)
-        return render_template('index.html', weight=weight, unit=unit)
-    else:
-        return render_template('index.html')
-
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
